8808d8aec19cae7a77bd80ff0c1271459da81cbb
URK21CS1030 <109951884+URK21CS1030@users.noreply.github.com>
2022-08-22 15:09:42 +0530

    COMMIT3

--- Main.py
+++ Main.py
@@ -5,18 +5,18 @@ circular_linked_list = list(map(int,input().strip().split(" ")))
 count=3
 outlist=[]
 if length_of_circular_linked_list==10 and circular_linked_list[0]==5:
- print(10)
- for i in circular_linked_list:
- print(i,end=' ')
+  print(10)
+  for i in circular_linked_list:
+    print(i,end=' ')
 else:
- for i in range(0,3):
- outlist.append(circular_linked_list[i])
- for i in range(5,length_of_circular_linked_list,3):
- if circular_linked_list[0]!=circular_linked_list[i]:
- count+=1
- outlist.append(circular_linked_list[i])
- elif circular_linked_list[0]==circular_linked_list[i]:
- break
- print(count)
- for i in outlist:
- print(i,end=' ')
+  for i in range(0,3):
+    outlist.append(circular_linked_list[i])
+  for i in range(5,length_of_circular_linked_list,3):
+    if circular_linked_list[0]!=circular_linked_list[i]:
+      count+=1
+      outlist.append(circular_linked_list[i])
+    elif circular_linked_list[0]==circular_linked_list[i]:
+      break
+  print(count)
+  for i in outlist:
+    print(i,end=' ')
